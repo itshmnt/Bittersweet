1a92ce20bfa9fbb4ddc65979b763ec288f652213
itshmnt <hemantxs5@gmail.com>
2021-12-18 15:26:31 +0530

    adding Delete and Get for User

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 
 //Update user
 router.put("/:id", async (req,res) => {
-    if (req.body.userId===req.params.id || req.user.isAdmin) {
+    if (req.body.userId===req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -25,10 +25,29 @@ router.put("/:id", async (req,res) => {
 })
 
 //Delete user
-
+router.delete("/:id", async (req,res) => {
+    if (req.body.userId===req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted!");
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json("You can delete only your account!")
+    }
+})
 
 //Get a user
-
+router.get("/:id", async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const {password, updatedAt, ...other} = user._doc;
+        res.status(200).json(other);
+    } catch(err) {
+        return res.status(500).json(err);
+    }
+})
 
 //Follow a user
 
