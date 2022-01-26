a1046ed2847b69368d910523a996ab46f2fda460
itshmnt <hemantxs5@gmail.com>
2022-01-26 18:04:56 +0530

    adding File upload route

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
 
 const app = express();
 
@@ -15,10 +17,30 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true}, ()=> {
     console.log("Connected to MongoDB!");
 } );
 
+app.use("/images", express.static(path.join(__dirname, "public/images")))
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req,res) => {
+    try {
+        return res.status(200).json("File uploaded successfully!");
+    } catch(err) {
+        alert(err);
+    }
+})
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
